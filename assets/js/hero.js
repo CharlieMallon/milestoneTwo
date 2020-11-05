3eafd45debd7c0a577bf29860166ebdae5960001
CharlieMallon <penguin47@hotmail.co.uk>
2020-11-05 12:30:50 +0000

    hero moving based on key strokes

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,14 +1,17 @@
+import {getInputDirection} from "/assets/js/input.js"
+
 export const HERO_SPEED = 2
 const heroBody = [{ x:11, y:11}]
 
 export function update () {
+    const inputDirection = getInputDirection()
 
     for( let i = heroBody.length -2; i >= 0; i--){
         heroBody[i + 1] = {...heroBody[i]}
     }
 
-    heroBody[0].x += 1
-    heroBody[0].y += 0
+    heroBody[0].x += inputDirection.x
+    heroBody[0].y += inputDirection.y
 }
 
 export function draw () {
