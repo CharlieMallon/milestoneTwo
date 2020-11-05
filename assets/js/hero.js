7858f0990c2517ef2ad9179436519ed723b18363
CharlieMallon <penguin47@hotmail.co.uk>
2020-11-05 12:20:30 +0000

    Move hero on gameBoard

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -3,6 +3,12 @@ const heroBody = [{ x:11, y:11}]
 
 export function update () {
 
+    for( let i = heroBody.length -2; i >= 0; i--){
+        heroBody[i + 1] = {...heroBody[i]}
+    }
+
+    heroBody[0].x += 1
+    heroBody[0].y += 0
 }
 
 export function draw () {
