1e0d6d91287c94cd656b85f4c4c1ea839676b5a5
CharlieMallon <penguin47@hotmail.co.uk>
2020-11-08 13:03:40 +0000

    added start and end squares

--- assets/js/board.js
+++ assets/js/board.js
@@ -11,20 +11,53 @@ export const wallBody = [
     { x:19, y:3, w:1, h:19}
     ]
 
+export const startSquare = [{ x:2, y:2 }]
+
+export const endSquare = [{ x:21, y:21 }]
+
 export function update () {
 
 }
 
 export function draw (gameBoard) {
-    wallBody.forEach(wall => {
-        const wallElement = document.createElement('div')
+    walls(gameBoard)
+    start (gameBoard)
+    end (gameBoard)
+}
 
-        wallElement.style.gridRowStart = wall.y
-        wallElement.style.gridRowEnd = `span ${wall.h}`
-        wallElement.style.gridColumnStart = wall.x
-        wallElement.style.gridColumnEnd = `span ${wall.w}`
-        wallElement.classList.add('wall')
+function walls(gameBoard){
+    wallBody.forEach(wall => {
+    const wallElement = document.createElement('div')
+
+    wallElement.style.gridRowStart = wall.y
+    wallElement.style.gridRowEnd = `span ${wall.h}`
+    wallElement.style.gridColumnStart = wall.x
+    wallElement.style.gridColumnEnd = `span ${wall.w}`
+    wallElement.classList.add('wall')
+    
+    gameBoard.appendChild(wallElement)
+})}
+
+function start(gameBoard){
+    startSquare.forEach(start => {
+        const startElement = document.createElement('div')
+
+        startElement.style.gridRowStart = start.y
+        startElement.style.gridColumnStart = start.x
+        startElement.classList.add('start')
         
-        gameBoard.appendChild(wallElement)
+        gameBoard.appendChild(startElement)
+    })
+}
+
+function end(gameBoard){
+    endSquare.forEach(end => {
+        const endElement = document.createElement('div')
+
+        endElement.style.gridRowStart = end.y
+        endElement.style.gridColumnStart = end.x
+        endElement.classList.add('end')
+        
+        gameBoard.appendChild(endElement)
     })
 }
