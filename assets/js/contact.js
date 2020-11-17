34cb29020626429ea1c5d91b5cc89e6312bfb7bc
CharlieMallon <penguin47@hotmail.co.uk>
2020-11-17 13:53:20 +0000

    added css

---
+++ assets/js/contact.js
@@ -0,0 +1,22 @@
+// //----------About the Developer----------//
+
+function showMore(){
+    
+}
+
+// // contact
+// function sendMail(contactForm){
+//     emailjs.send("Rosie_Project","template_rosie", {
+//         "project_request": contactForm.projectsummary.value,
+//         "from_name": contactForm.name.value,
+//         "from_email": contactForm.emailaddress.value,
+//     })
+//     .then(
+//         function(response){
+//             console.log("SUCCESS", response);
+//         },
+//         function(error){
+//             console.log("error", error);
+//     });
+//     return false;
+// }
