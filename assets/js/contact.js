715754ac395ff5830c9bc6990a81765f007bed7a
CharlieMallon <penguin47@hotmail.co.uk>
2020-11-17 16:59:46 +0000

    added the send email functionality

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,22 +1,35 @@
 // //----------About the Developer----------//
 
 function showMore(){
-    
+    document.getElementById("readMore").innerHTML = '<a href="javascript:showLess()" id="readMore">Read Less</a>'
+    document.getElementById("more").classList.remove('hidden-text')
+}
+
+function showLess(){
+    document.getElementById("readMore").innerHTML = '<a href="javascript:showMore()" class="not-tab" id="readMore">Read More</a>'
+    document.getElementById("more").classList.add('hidden-text')
 }
 
 // // contact
-// function sendMail(contactForm){
-//     emailjs.send("Rosie_Project","template_rosie", {
-//         "project_request": contactForm.projectsummary.value,
-//         "from_name": contactForm.name.value,
-//         "from_email": contactForm.emailaddress.value,
-//     })
-//     .then(
-//         function(response){
-//             console.log("SUCCESS", response);
-//         },
-//         function(error){
-//             console.log("error", error);
-//     });
-//     return false;
-// }
+function sendMail(){
+    emailjs.send("iCloud-Charlie","template_maze", {
+        "message": document.getElementById("subject").value,
+        "from_name": document.getElementById("fullName").value,
+        "from_email": document.getElementById("emailAddress").value,
+    })
+    .then(
+        function(response){
+            console.log("SUCCESS", response);
+        },
+        function(error){
+            console.log("error", error);
+    });
+    return false;
+}
+
+window.onload = function() {
+    document.getElementById('send').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('template_maze', 'form', this);
+    });
+}
